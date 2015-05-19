eb1111517e421480ea222a47addef09fb3355e4e
jssalamanca1967 <js.salamanca1967@uniandes.edu.co>
2015-05-18 19:05:00 -0500

    Asd

---
+++ hospitalKennedy.interfaz/src/main/webapp/js/AngularControllers/app.js
@@ -0,0 +1,18 @@
+(function(){var hospitalKennedy= angular.module('hospitalKennedy',[]);
+hospitalKennedy.directive('toolbar', function(){
+        return{
+            restrict:'E',
+            templateUrl: 'partials/toolbar.html',
+            controller:function(){
+                this.tab=0;
+                this.selectTab=function(setTab){
+                    this.tab=setTab;
+                };
+                this.isSelected=function(tabParam){
+                    return this.tab===tabParam;
+                };
+            },
+            controllerAs:'toolbar'
+        };
+    });
+})();
